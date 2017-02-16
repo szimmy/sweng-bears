9fafc45bf59d6235c3a532cb7894df9252c2932a
Sean Zimmerman <seanwzimmerman@gmail.com>
2017-02-16 16:08:02 -0500

    Added class with main method as a rough starting point for development.

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+/**
+ * Contains the main method of the program. Takes in file(s) and runs selected reports on them.
+ *
+ * @author Sean Zimmerman
+ */
+public class Controller {
+
+    public static void main(String args[]) {
+        
+    }
+}
