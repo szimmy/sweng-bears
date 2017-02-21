6eb4b3932fa3d1f93198b7b970a7d1df886c9ece
Sean Zimmerman <seanwzimmerman@gmail.com>
2017-02-21 01:02:43 -0500

    Add functionality for a file chooser and detection for a valid file extension

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
 /**
  * Contains the main method of the program. Takes in file(s) and runs selected reports on them.
  *
@@ -6,6 +10,33 @@
 public class Controller {
 
     public static void main(String args[]) {
-        
+        chooseFile();
+    }
+
+    private static void chooseFile() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "CMS-2Y", "cts", "ct2");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(new JFrame());
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " +
+                    chooser.getSelectedFile().getName());
+        }
+
+        String extension = "";
+
+        int i = chooser.getSelectedFile().getAbsolutePath().lastIndexOf('.');
+        int p = Math.max(chooser.getSelectedFile().getAbsolutePath().lastIndexOf('/'),
+                chooser.getSelectedFile().getAbsolutePath().lastIndexOf('\\'));
+
+        if (i > 0 && i > p) {
+            extension = chooser.getSelectedFile().getAbsolutePath().substring(i+1);
+        }
+
+        if(!extension.equals("cts") && !extension.equals("ct2")) {
+            System.out.println("File extension error: " + extension + " selected.");
+        }
     }
 }
