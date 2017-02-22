1922cc5f81597b3b158f3304a47ff4ada5411888
Sean Zimmerman <seanwzimmerman@gmail.com>
2017-02-22 16:49:55 -0500

    Added initial File Scanning including counting the number of lines.

---
+++ src/FileScanner.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Created by Zim on 2/22/2017.
+ */
+public class FileScanner {
+
+    private File file;
+    private Scan scan;
+    private HashMap<String, Integer> data;
+
+    public FileScanner(File file, Scan scan) {
+        this.file = file;
+        this.scan = scan;
+        this.data = new HashMap<>();
+    }
+
+    public HashMap<String, Integer> run() {
+        int numLines = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line
+                numLines++;
+            }
+        } catch(IOException ie) {
+        }
+
+        data.put("Lines", numLines);
+
+        return data;
+    }
+}
