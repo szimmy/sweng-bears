0e12b88168984a02288786b3944885e016e03ce5
JAdirzone <adirzonej0@students.rowan.edu>
2017-02-25 12:23:42 -0500

    Counts Lines, Comment Stmts, Comment Lines , CMS-2Y Other Stmts, CMS-2Y Other Comments, Direct Code Comments, Direct Code Other. Modified main loop for easier lines analysis.

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,16 @@
+import java.io.File;
 /**
  * Contains the main method of the program. Takes in file(s) and runs selected reports on them.
  *
  * @author Sean Zimmerman
  */
+
 public class Controller {
 
     public static void main(String args[]) {
-        
+        //Hardcoded test REMOVE
+        FileScanner fileScanner = new FileScanner(new File("C:/Users/Jay/Documents/SampleCMS2/TEST7.txt"),
+                new ReportContent());
+        System.out.println(fileScanner.run());
     }
 }
--- src/FileScanner.java
+++ src/FileScanner.java
@@ -21,18 +21,113 @@ public class FileScanner {
 
     public HashMap<String, Integer> run() {
         int numLines = 0;
+        int numCommentStmts = 0;
+        int numCommentLines = 0;
+        int numDirCommentsStmts = 0;
+        //This will need to be split into two in a later sprint.
+        int numDirOther = 0;
+        int numCMSOtherStmts = 0;
+        int numCMSOtherLines = 0;
+
+        boolean inDirectBlock = false;
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
+            String statement = "";
+
+            boolean stmtBegun = false;
+            int stmtBeginningLine = 0;
+
             while ((line = br.readLine()) != null) {
-                // process the line
                 numLines++;
+                //Change tabs into spaces
+                line = transformTabs(line);
+                //Take the first ten characters off. They are not used.
+                if(line.length() > 10){
+                    line = line.substring(10);
+                    if(stmtBegun == false){ //May incorrectly handle labels on blank lines proceeding statements.
+                        stmtBeginningLine = numLines;
+                        stmtBegun = true;
+                    }
+                }
+                else{
+                    line = "";
+
+                }
+                //TODO Pad the back with spaces. (?)
+                if(! inDirectBlock) {
+                    statement = statement.concat(trimDelim(line, '$'));
+                    if (statement.contains("$")) {  //TODO Change this to check if it ends with a dollar sign. (?)
+                        //Perform analysis of CMS-2Y statements here. (Not Direct Code, that is below.)
+                        if (getFirstToken(statement).equals("COMMENT")) {
+                            numCommentStmts++;
+                            numCommentLines += (numLines - stmtBeginningLine) + 1;
+                        }
+                        else if(getFirstToken(statement).equals("DIRECT") ||
+                                getFirstToken(statement).equals("DIRECT$")){ //I don't know if this would be legal
+                            numCMSOtherStmts++; //Test this line and the next
+                            numCMSOtherLines += (numLines - stmtBeginningLine) + 1;
+                            inDirectBlock = true;
+                        }else{
+                            numCMSOtherStmts++; //Test this line and the next
+                            numCMSOtherLines += (numLines - stmtBeginningLine) + 1;
+                        }
+                        //clear appropriate values
+                        statement = "";
+                        stmtBeginningLine = 0; //This seems like a dangerous default value, but should work.
+                        stmtBegun = false;
+                    }
+                }
+                else{ //current statement is in a direct code block
+                    statement = line;
+                    //Perform analysis of Direct Code statements here.
+                    if(getFirstToken(statement).equals(".")){
+                        numDirCommentsStmts++;
+                    }
+                    else if(getFirstToken(statement).equals("CMS-2") ||
+                            getFirstToken(statement).equals("CMS-2$")){
+                        //I'm counting the direct code block ending-delimiter as CMS-2, not direct code.
+                        inDirectBlock = false;
+                    }
+                    else{
+                        numDirOther++;
+                    }
+                    statement = "";
+                }
             }
         } catch(IOException ie) {
         }
 
         data.put("Lines", numLines);
+        data.put("Comment Statements", numCommentStmts);
+        data.put("Comment Lines", numCommentLines);
+        data.put("CMS-2Y Other Statements", numCMSOtherStmts);
+        data.put("CMS-2Y Other Lines", numCMSOtherLines);
+        data.put("Direct Code Comments", numDirCommentsStmts);
+        data.put("Direct Code Other", numDirOther);
 
         return data;
+    }
+
+    private String trimDelim(String line, char delimiter){ //Change the name of this, but not to trim
+        int delimiterIndex = line.indexOf(delimiter);
+        if(delimiterIndex == -1) {
+            return line;
+        }
+        return line.substring(0, delimiterIndex + 1);
+    }
+    //If it is possible that the first token isn't followed by a space, needs to be changed.
+    private String getFirstToken(String statement){
+            return statement.trim().substring(0, statement.trim().indexOf(" ")).trim();
+    }
+
+    private String transformTabs(String string){
+        String[] segments = string.split("\t");
+        String result = "";
+        for(String seg : segments){
+            result = result.concat(seg + "    ");
+        }
+        result = result.substring(0, result.length() - 4);
+        return result;
     }
 }
