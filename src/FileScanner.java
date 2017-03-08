f996139b14332492b24e7b34ff908eef323fdc82
Alexander Mendelsohn <superparkourio@gmail.com>
2017-03-08 12:11:00 -0500

    The program will now count data statements and data lines by looking for VRBL, TABLE, END-TABLE, FIELD, and ITEM-AREA.

--- src/FileScanner.java
+++ src/FileScanner.java
@@ -36,6 +36,8 @@ public class FileScanner {
         int numLines = 0;
         int numCommentStmts = 0;
         int numCommentLines = 0;
+        int numDataStmts = 0;
+        int numDataLines = 0;
         int numGotoStmts = 0;
         int numGotoLines = 0;
         int numDirCommentsStmts = 0;
@@ -81,6 +83,13 @@ public class FileScanner {
                         if (getFirstToken(statement).equals("COMMENT")) {
                             numCommentStmts++;
                             numCommentLines += (numLines - stmtBeginningLine) + 1;
+                        } else if (getFirstToken(statement).equals("VRBL")
+                                || getFirstToken(statement).equals("TABLE")
+                                || getFirstToken(statement).equals("END-TABLE")
+                                || getFirstToken(statement).equals("FIELD")
+                                || getFirstToken(statement).equals("ITEM-AREA")) {
+                            numDataStmts++;
+                            numDataLines += (numLines - stmtBeginningLine) + 1;
                         } else if (getFirstToken(statement).equals("GOTO")) {
                             numGotoStmts++;
                             numGotoLines += (numLines - stmtBeginningLine) + 1;
@@ -123,6 +132,8 @@ public class FileScanner {
         data.put("Lines", numLines);
         data.put("Comment Statements", numCommentStmts);
         data.put("Comment Lines", numCommentLines);
+        data.put("Data Statements", numDataStmts);
+        data.put("Data Lines", numDataLines);
         data.put("Goto Statements", numGotoStmts);
         data.put("Goto Lines", numGotoLines);
         data.put("CMS-2Y Other Statements", numCMSOtherStmts);
