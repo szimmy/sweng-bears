e0ffe1a555713b1a3c7d4f9eb784c3fe7cef4291
Jeffreylehman94 <jeffreylehman94@gmail.com>
2017-03-29 15:27:03 -0400

    Tallys lines for HighLevelData now

--- src/Scans/HighLevelDataScanner.java
+++ src/Scans/HighLevelDataScanner.java
@@ -2,6 +2,7 @@ package Scans;
 
 import CMS2Statements.Statement;
 import Reports.Entry;
+
 import java.util.ArrayList;
 
 public class HighLevelDataScanner extends LineScan {
@@ -10,26 +11,26 @@ public class HighLevelDataScanner extends LineScan {
     public HighLevelDataScanner() {
         KEYWORD = "Data";
         count = 0;
-        lineCount=0;
+        lineCount = 0;
     }
 
     // DID NOT INCLUDE
     // TABLE, END-TABLE, ITEM-AREA, TYPE, END-TYPE, DATA, SUB-TABLE, SUBTABLE
     // UNSURE IF INCLUDED UNDER DATA STMTS
-    public void scan(Statement statement){
-        String s=getFirstToken(statement.getText());
-        if(!statement.isDirectCode()){
-            if(s.equals("VRBL") || s.equals("FIELD")) {
+    public void scan(Statement statement) {
+        String s = getFirstToken(statement.getText());
+        if (!statement.isDirectCode()) {
+            if (s.equals("VRBL") || s.equals("FIELD")) {
                 count++;
                 tallyLines(statement);
             }
         }
-
     }
-
+/*
     public ArrayList<Entry> getData() {
         ArrayList<Entry> data = super.getData();
         data.add(new Entry(KEYWORD + " Lines", lineCount));
         return data;
     }
+    */
 }
