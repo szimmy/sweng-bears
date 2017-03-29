a631f664caa5c147348878f5349dd1d0a3e41a36
Jeffreylehman94 <jeffreylehman94@gmail.com>
2017-03-29 14:04:05 -0400

    Now includes just the HighLevelDataScanner, removed delimScanner

--- src/Scans/TotalDelimtStmtsScanner.java
+++ src/Scans/TotalDelimtStmtsScanner.java
@@ -4,21 +4,6 @@ import CMS2Statements.Statement;
 
 public class TotalDelimtStmtsScanner extends Scan {
 
-    // Array containing all Delimiter Statements keywords in the CMS2 language
-    // The end of the Delimiter Statements keywords are commented out, assuming the do not need to be counted.
-    // If they do, uncomment them back in.
-    String[] delimiters = new String[]{
-            "SYSTEM",// "END-SYSTEM",
-            "SYS-DD",// "END-SYS-DD",
-            "SYS-PROC",// "END-SYS-PROC",
-            "LOC-DD",// "END-LOC-DD",
-            "PROCEDURE",// "END-PROC",
-            "EXEC-PROC",// "END-PROC",
-            "FUNCTION",// "END-FUNCTION",
-            "SYS-PROC-REN",// "END-SYS-PROC",
-            "AUTO-DD",// "END-AUTO-DD",
-            "HEAD"//, "END-HEAD"
-    };
 
     public TotalDelimtStmtsScanner() {
         KEYWORD = "Delim";
@@ -34,14 +19,5 @@ public class TotalDelimtStmtsScanner extends Scan {
      * @param statement The statement to be scanned
      */
     public void scan(Statement statement) {
-        String text = getFirstToken(statement.getText());
-        if (!statement.isDirectCode()) {
-            for (String s : delimiters) {
-                if (text.equals(s)) {
-                    count++;
-                    break;
-                }
-            }
-        }
     }
 }
