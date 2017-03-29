989aa09511dd116bc16963d5f2da6d25a414ee5a
Jeffreylehman94 <jeffreylehman94@gmail.com>
2017-03-29 15:48:54 -0400

    Tallys lines for HighLevelData now

--- src/Scans/HighLevelDataScanner.java
+++ src/Scans/HighLevelDataScanner.java
@@ -14,23 +14,20 @@ public class HighLevelDataScanner extends LineScan {
         lineCount = 0;
     }
 
-    // DID NOT INCLUDE
+    // DID INCLUDE
     // TABLE, END-TABLE, ITEM-AREA, TYPE, END-TYPE, DATA, SUB-TABLE, SUBTABLE
     // UNSURE IF INCLUDED UNDER DATA STMTS
     public void scan(Statement statement) {
         String s = getFirstToken(statement.getText());
         if (!statement.isDirectCode()) {
-            if (s.equals("VRBL") || s.equals("FIELD")) {
+                if (
+                        s.equals("VRBL") || s.equals("FIELD") || s.equals("TABLE") ||
+                            s.equals("END-TABLE") || s.equals("ITEM-AREA") || s.equals("TYPE") ||
+                            s.equals("END-TYPE") || s.equals("DATA") || s.equals("SUB-TABLE") ||
+                            s.equals("SUBTABLE")) {
                 count++;
                 tallyLines(statement);
             }
         }
     }
-/*
-    public ArrayList<Entry> getData() {
-        ArrayList<Entry> data = super.getData();
-        data.add(new Entry(KEYWORD + " Lines", lineCount));
-        return data;
-    }
-    */
 }
