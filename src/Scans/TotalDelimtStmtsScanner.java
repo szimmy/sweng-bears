24dfeb05e9835b6b605b9772548d66f6e6114563
Jeffreylehman94 <jeffreylehman94@gmail.com>
2017-03-29 13:36:44 -0400

    Used getFirstToken(); to fix an error and only searched for the start of delim statements

--- src/Scans/TotalDelimtStmtsScanner.java
+++ src/Scans/TotalDelimtStmtsScanner.java
@@ -4,12 +4,41 @@ import CMS2Statements.Statement;
 
 public class TotalDelimtStmtsScanner extends Scan {
 
+    // Array containing all Delimiters in the CMS2 language
+    String[] delimiters = new String[]{
+            "SYSTEM",// "END-SYSTEM",
+            "SYS-DD",// "END-SYS-DD",
+            "SYS-PROC",// "END-SYS-PROC",
+            "LOC-DD",// "END-LOC-DD",
+            "PROCEDURE",// "END-PROC",
+            "EXEC-PROC",// "END-PROC",
+            "FUNCTION",// "END-FUNCTION",
+            "SYS-PROC-REN",// "END-SYS-PROC",
+            "AUTO-DD",// "END-AUTO-DD",
+            "HEAD"//, "END-HEAD"
+    };
+
     public TotalDelimtStmtsScanner() {
-        KEYWORD = "Delimt";
+        KEYWORD = "Delim";
         count = 0;
     }
 
+    /**
+     * Delimiters are used to start and end parts of a CMS2 code, therefore it will only be written in the start or
+     * end of a statement; meaning there will only be one per line.
+     * Uses .contains() for this reason
+     *
+     * @param statement The statement to be scanned
+     */
     public void scan(Statement statement) {
-        // TODO
+        String text = getFirstToken(statement.getText());
+        if (!statement.isDirectCode()) {
+            for (String s : delimiters) {
+                if (text.equals(s)) {
+                    count++;
+                    break;
+                }
+            }
+        }
     }
 }
