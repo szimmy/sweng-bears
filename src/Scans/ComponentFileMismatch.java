cee99830a1f30ccccb03f3ef74f0a78d2b36e9c0
Alexander Mendelsohn <superparkourio@gmail.com>
2017-04-29 17:08:39 -0400

    ComponentFileMismatch Done The program can now tell if the name of a SYSTEM doesn't match the file's name

--- src/Scans/ComponentFileMismatch.java
+++ src/Scans/ComponentFileMismatch.java
@@ -1,17 +1,31 @@
 package Scans;
 
 import CMS2Statements.Statement;
+import Controller.Controller;
 
 /**
- * Created by Zim on 4/15/2017.
+ * This class scans code for Component Name/File Mismatches.
  */
 public class ComponentFileMismatch extends Scan {
+
+    /**
+     * Constructor for ComponentFileMismatch.
+     */
     public ComponentFileMismatch(){
         KEYWORD = "File Name";
         count = 0;
     }
 
+    /**
+     * Scans a statement and counts it if it is a SYSTEM statement whose name doesn't match the file's name.
+     * (If the SYSTEM statement doesn't have a name, that's a mismatch as well.)
+     * @param statement The statement to be scanned
+     */
     public void scan(Statement statement){
-
+        String s = statement.getText();
+        if (!getFirstToken(s).equals("COMMENT") && !getFirstToken(s).equals("END-SYSTEM")&& s.contains("SYSTEM")
+                && !getFirstToken(s).toUpperCase().equals(Controller.currentFileName.toUpperCase())) {
+            count++;
+        }
     }
 }
