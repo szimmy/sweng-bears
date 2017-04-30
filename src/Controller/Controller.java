ae3ecbe893deaef36cbf2e3f5bd73c2b15c151df
Alexander Mendelsohn <superparkourio@gmail.com>
2017-04-29 22:15:04 -0400

    LinuxTestingDone Had to make the \\ in Controller.getFiles() into a / so the command line would work on Linux.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -137,7 +137,7 @@ public class Controller {
         }
         File[] files = new File[args.length-1];
         for (int i = 1; i < args.length; i++) {
-            files[i-1] = new File(DEFAULTDIRECTORY + "\\" + args[i]);
+            files[i-1] = new File(DEFAULTDIRECTORY + "/" + args[i]);
         }
         return files;
     }
