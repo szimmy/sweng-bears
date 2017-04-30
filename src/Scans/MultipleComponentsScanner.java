a1987b6c99b699c21ef8968966668d7e56f488c6
JAdirzone <adirzonej0@students.rowan.edu>
2017-04-30 15:57:54 -0400

    Added MultipleComponentScanner. Should work with getSecondToken.

--- src/Scans/MultipleComponentsScanner.java
+++ src/Scans/MultipleComponentsScanner.java
@@ -6,12 +6,22 @@ import CMS2Statements.Statement;
  * Created by Zim on 4/15/2017.
  */
 public class MultipleComponentsScanner extends Scan {
+
+    private boolean inComponent = false;
+
     public MultipleComponentsScanner(){
         KEYWORD = "Mult Comp";
         count = 0;
     }
 
     public void scan(Statement statement){
+        if(!inComponent && getSecondToken(statement.getText()).equalsIgnoreCase("system")){
+            inComponent = true;
+        }else if(inComponent && getSecondToken(statement.getText()).equalsIgnoreCase("end-system")){
+            inComponent = false;
+            count += 1; //Does this need to count how many, or just confirm if there is more than one?
+        }
+
 
     }
 }
