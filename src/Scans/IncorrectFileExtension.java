d587dc85b884ce6a76024fc97e25f91d9632eb9e
JAdirzone <adirzonej0@students.rowan.edu>
2017-05-01 13:27:21 -0400

    Added incorrect file extension scanner. Requires a change to SourceReview to work.

--- src/Scans/IncorrectFileExtension.java
+++ src/Scans/IncorrectFileExtension.java
@@ -14,4 +14,14 @@ public class IncorrectFileExtension extends Scan {
     public void scan(Statement statement){
 
     }
+
+    /**
+     * This scanner does not check statements, it checks the extension of a file once.
+     * Therefore this is necessary and the scan function is not.
+     */
+    public void checkExtension(String extension){
+        if(extension.equals("txt")){
+            count++;
+        }
+    }
 }
